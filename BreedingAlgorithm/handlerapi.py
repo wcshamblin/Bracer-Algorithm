79adceb985c27cc705df7fd68747fea9660e8fa1
wcshamblin <will@shamblin.org>
2021-01-02 13:32:29 -0500

    Debugging

--- BreedingAlgorithm/handlerapi.py
+++ BreedingAlgorithm/handlerapi.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, redirect, url_for,request
 from flask import make_response
+from json import dumps
+
+from boxbreed import boxbreed
 
 app = Flask(__name__)
+
+# POSTs must have Content-Type: application/json header
 
 @app.route("/")
 def slash():
     return "Use route /gtlbreed/ or /boxbreed/"
 
-@app.route("/gtlbreed", methods=['GET', 'POST'])
+@app.route("/gtlbreed/", methods=['GET', 'POST'])
 def gtlroute():
     if request.method == 'GET':
         return("No data to return - need POST")
@@ -15,12 +20,12 @@ def gtlroute():
         # Get data from gtl breed algo
         return("Data")
 
-@app.route('/boxbreed', methods=['GET', 'POST'])
+@app.route('/boxbreed/', methods=['GET', 'POST'])
 def boxroute():
     if request.method == 'GET':
         return("No data to return - need POST")
     if request.method == 'POST':
         # Get data from box breed algo
-        return("Data")
+        return(boxbreed(request.json))
 
-app.run()
+app.run(debug=True)
