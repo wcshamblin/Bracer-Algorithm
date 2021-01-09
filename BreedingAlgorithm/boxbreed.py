587953e98158b19d032b6074bd57c362bc1195bf
wcshamblin <will@shamblin.org>
2021-01-08 20:28:37 -0500

    I think it works all the way I just want to sleep

--- BreedingAlgorithm/boxbreed.py
+++ BreedingAlgorithm/boxbreed.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import copy
 from itertools import combinations_with_replacement as combinations_wr
+from itertools import permutations
+from itertools import chain
 from collections import OrderedDict
 from collections import Counter
 import math
@@ -24,7 +26,8 @@ def get_parents(child, sharedict):
 def combinate(plevel, lnum):
     # Can't have more than 2 1s because you cannot have more than two braces in any particular 2 pokemon breed
     # Combinations after first value must be part of a previous level
-    return([p for p in combinations_wr(list(range(1,int((2**(lnum-1)/2))+1)), r=lnum) if sum(p)==(2**(lnum-1)) and p.count(1) <=2])
+    return(list(chain.from_iterable([list(set([i for i in permutations(p)])) for p in [p for p in combinations_wr(list(range(1,int((2**(lnum-1)/2))+1)), r=lnum) if sum(p)==(2**(lnum-1)) and p.count(1) <=2]])))
+    #return([p for p in combinations_wr(list(range(1,int((2**(lnum-1)/2))+1)), r=lnum) if sum(p)==(2**(lnum-1)) and p.count(1) <=2])
 
 # List distance for finding optimal distribution from input
 def listdistance(l1, l2):
@@ -45,37 +48,20 @@ def treegen(distdict, target):
             branched_children, sharedict = get_parents(child, sharedict)
             for branched_child in branched_children:
                 treedict[level].append(branched_child)
-
     return(treedict)
-
 
 # Breed function
 def boxbreed(data):
     # Data preprocessing
-    # Count ivs to get inp
-    inp = {"hp": 0,"atk": 0, "def": 0,"spa": 0,"spd": 0,"spe": 0}
-
     breederlist = []
     for breeder in data["breeders"]:
-        breederlist.append([iv for iv, state in breeder["ivs"].items() if state == "True"])
-
-    # This is breaking apart 2xs+
-    for breeder in data["breeders"]:
-        for iv, state in breeder["ivs"].items():
-            if state == "True":
-                inp[iv]+=1 
+        breederlist.append(sorted([iv for iv, state in breeder["ivs"].items() if state == True]))
 
     # Set target
     target = []
     for iv, state in data["target"]["ivs"].items():
-        if state != "False":
+        if state != False:
             target.append(iv)
-
-    inp = {"hp": 20,"atk": 20, "def": 20,"spa": 20,"spd": 20,"spe": 20}
-    inp = {iv:state for iv, state in inp.items() if state != 0 and iv in target}
-    inp = OrderedDict(sorted(inp.items(), key=lambda t: t[1]))
-    if len(inp) <= 1:
-        return("Not enough breeders")
 
     distributions = {} # key:[list] of (sets)
     # Start with 1
@@ -86,10 +72,11 @@ def boxbreed(data):
         distributions[lnum] = combinate(level, lnum)
         lnum+=1
 
+    # Generate all trees from distributions
     treelist = []
-    for distribution in distributions[len(target)]: # 5x level
+    for distribution in distributions[len(target)]:
         distdict={}
-        for value, stat in zip(distribution, list(inp.keys())): # Low -> high
+        for value, stat in zip(distribution, list(target)): # Low -> high
             distdict[stat] = value
         distdict=OrderedDict(reversed(list(distdict.items()))) # Reverse so we can iterate high -> low
         tree = treegen(distdict, target)
@@ -98,12 +85,19 @@ def boxbreed(data):
     # Find the best distribution and tree for the input
     treedict = {} # treevalue: tree
     for tree in treelist:
+        breedercompare = breederlist.copy() # This resets tree by tree to make sure we don't re-use any breeders
         treevalue = 0
         for level, breeders in tree.items():
             for breeder in breeders:
-                if breeder in breederlist:
-                    treevalue += 2**(len(breeder)-1)
-        if len(treedict)>0:
+                # print("Breeder, breeders", breeder, breeders)
+                if sorted(breeder) in breedercompare: # Both are sorted
+                    treevalue += (2**(len(breeder)-1))
+                    if len(breeder) == 2:
+                        treevalue += 2
+                    breedercompare.remove(sorted(breeder)) # If we used a breeder, remove it from the pool
+
+
+        if len(treedict) > 0:
             if treevalue > max(treedict.keys()):
                 del treedict[max(treedict.keys())]
                 treedict[treevalue] = tree
@@ -118,19 +112,19 @@ def boxbreed(data):
         for breeder in level:
             match = []
             for targetbreeder in data["breeders"]:
-                if sorted(breeder) == sorted([iv for iv, state in targetbreeder["ivs"].items() if state == "True"]):
+                if sorted(breeder) == sorted([iv for iv, state in targetbreeder["ivs"].items() if state == True]):
                     match = [targetbreeder]
+                    data["breeders"].remove(targetbreeder)
             if not match:
                 match = [breeder]
             if treelevel in treejson.keys():
                 treejson[treelevel] += match
             else:
                 treejson[treelevel] = match
-
-    return(dumps(treejson, indent=4, sort_keys=True))
+    return(treejson)
 
 
 if __name__ == '__main__':
     with open("test.json") as data:
         data = load(data)
-    print(boxbreed(data))
+    print(dumps(boxbreed(data)))
