ec74d7703223ccd9d1e6c3f10fa363780d89d759
Will Shamblin <wcshamblin@gmail.com>
2021-02-25 14:06:40 -0500

    Naive first commit of cleaned treegen logic

--- BreedingAlgorithm/boxbreed.py
+++ BreedingAlgorithm/boxbreed.py
@@ -6,7 +6,7 @@ from json import dumps, load
 from functools import reduce
 from multiprocessing import Pool
 from multiprocessing import cpu_count
-from boxbreedutils import get_parents, combinate, breedercompat, jsonify, findaddbreeder
+from boxbreedutils import get_parents, combinate, breedercompat, jsonify, findaddbreeder, convertbreeder, isinbreeders
 
 # Generate tree from distribution and target - this is poorly optimized
 def treegen(distdict, target, breederlist):
@@ -23,37 +23,48 @@ def treegen(distdict, target, breederlist):
             if not child: # Is an empty list placeholder - fill placeholders below
                 treedict[level].append([])
                 treedict[level].append([])
-                continue
+                continue # Restart loop
 
-            if type(child) != dict: # Isn't a breeder - we can split it!
-                branched_children, sharedict = get_parents(child, sharedict) # Split
-                firstisbreeder = False
+            # if type(child) != dict: # Isn't a breeder - we can split it!
+            if not child["breeder"]: # Isn't a breeder - we can split it!
+                child = convertbreeder(child, isbreeder=False) # dict -> list
+                branched_parents, sharedict = get_parents(child, sharedict) # Split
+                branched_parents = [convertbreeder(parent, isbreeder=False) for parent in branched_parents] # Convert split back into dict
 
-                if branched_children[0] in [sorted([iv for iv, state in breeder["ivs"].items() if state == True]) for breeder in tempbreeders]: # is the first child a breeder?
-                    firstisbreeder = True
-                    # Find fancybreeder, remove from pool
-                    findaddbreeder(treedict[level], branched_children, tempbreeders, 0)
-                    firstbreeder = treedict[level][-1]
+                firstisbreeder, firstbreeder = isinbreeders(branched_parents[0], tempbreeders)
+                if firstisbreeder:
+                    tempbreeders.remove(firstbreeder)
+                treedict[level].append(firstbreeder)
+
+                if firstisbreeder: # They're both breeders, we need to do compat checking
+                    addedcompat, secondbreeder = findcompatbreeder(branched_parents[1], firstbreeder, tempbreeders)
+                    if addedcompat:
+                        tempbreeders.remove(secondbreeder)
+                    treedict[level].append(secondbreeder)
 
                 else:
-                    treedict[level].append(branched_children[0])
+                    # Add without compat
+                    secondisbreeder, secondbreeder = isinbreeders(branched_parents[1], tempbreeders)
+                    if secondisbreeder:
+                        tempbreeders.remove(secondbreeder)
+                    treedict[level].append(secondbreeder)
 
-                addedcompat = False
-                if branched_children[1] in [sorted([iv for iv, state in breeder["ivs"].items() if state == True]) for breeder in tempbreeders]: # is the second child a breeder?
-                    if firstisbreeder: # First is already a breeder, need to match second against it
-                        for breeder in tempbreeders:  # Find a second fancy breeder that matches the first - if none found, add simplebreeder
-                            if branched_children[1] == sorted([iv for iv, state in breeder["ivs"].items() if state == True]):
-                                if breedercompat(firstbreeder, breeder): # Are the breeders compatable
-                                    addedcompat = True
-                                    treedict[level].append(breeder)
-                                    tempbreeders.remove(breeder)
+                # addedcompat = False
+                # if branched_parents[1] in [sorted([iv for iv, state in breeder["ivs"].items() if state == True]) for breeder in tempbreeders]: # is the second child a breeder?
+                #     if firstisbreeder: # First is already a breeder, need to match second against it
+                #         for breeder in tempbreeders:  # Find a second fancy breeder that matches the first - if none found, add simplebreeder
+                #             if branched_parents[1] == sorted([iv for iv, state in breeder["ivs"].items() if state == True]):
+                #                 if breedercompat(firstbreeder, breeder): # Are the breeders compatable
+                #                     addedcompat = True
+                #                     treedict[level].append(breeder)
+                #                     tempbreeders.remove(breeder)
 
-                    else: # First isn't a breeder - we can just add another fancybreeder without compat checking
-                        findaddbreeder(treedict[level], branched_children, tempbreeders, 1)
-                        addedcompat = True
+                #     else: # First isn't a breeder - we can just add another fancybreeder without compat checking
+                #         findaddbreeder(treedict[level], branched_parents, tempbreeders, 1)
+                #         addedcompat = True
 
-                if not addedcompat: # If we don't have a matching breeder OR we don't have a compatable breeder
-                    treedict[level].append(branched_children[1])
+                # if not addedcompat: # If we don't have a matching breeder OR we don't have a compatable breeder
+                #     treedict[level].append(branched_parents[1])
 
             else: # Is a breeder - don't split it, add placeholders to branch
                 treedict[level].append([])
@@ -76,10 +87,12 @@ def boxbreed(data):
     breederlist = data["breeders"]
 
     # Set target
-    target = []
+    target = data["target"]
+    simpletarget = []
     for iv, state in data["target"]["ivs"].items():
         if state is not False:
-            target.append(iv)
+            simpletarget.append(iv)
+    
 
     distributions = {} # key:[list] of (sets)
     # Start with 1
@@ -94,9 +107,9 @@ def boxbreed(data):
     procpool = Pool(cpu_count()) # Set up processing pool
     args = []
     treedict = {}
-    for distribution in distributions[len(target)]:
+    for distribution in distributions[len(simpletarget)]:
         distdict={}
-        for value, stat in zip(distribution, list(target)): # Low -> high
+        for value, stat in zip(distribution, list(simpletarget)): # Low -> high
             distdict[stat] = value
         distdict=OrderedDict(reversed(list(distdict.items()))) # Reverse so we can iterate high -> low
         tree, perfect, score = treegen(distdict, target, breederlist)
