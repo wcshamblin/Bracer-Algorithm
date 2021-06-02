7263aa3a21b3a366449f6f45402b52b039e1f2b3
wcshamblin <will@shamblin.org>
2021-06-01 23:02:10 -0400

    Renaming for importability

--- handlerapi.py
+++ handlerapi.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from flask_cors import CORS, cross_origin
 from json import dumps
 
-from boxbreed import boxbreed
+from Bracer-Algorithm import boxbreed
 
 app = Flask(__name__)
 cors = CORS(app)
