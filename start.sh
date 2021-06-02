7f7775cab18c2728fe1066726e7314477b4e6f55
wcshamblin <will@shamblin.org>
2021-06-01 23:08:29 -0400

    Renaming yet again

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-tmux new-session -d -n "Bracer_AlgorithmAPI" gunicorn3 -w 4 -b 127.0.0.1:5001 --chdir /home/mcserver/Bracer/Bracer_Algorithm/ handlerapi:app
+tmux new-session -d -n "Bracer-AlgorithmAPI" gunicorn3 -w 4 -b 127.0.0.1:5001 --chdir /home/mcserver/Bracer/Bracer-Algorithm/ handlerapi:app
