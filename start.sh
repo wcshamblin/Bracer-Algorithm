99e82481fa869aea636c8ac6f8a1e07c4d05966a
Will Shamblin <will@shamblin.org>
2022-01-21 15:40:34 -0500

    Timeout

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-tmux new-session -d -n "Bracer-AlgorithmAPI" gunicorn3 -w 4 -b 127.0.0.1:5001 --chdir /home/mcserver/Bracer/Bracer-Algorithm/Bracer-Algorithm handlerapi:app
+tmux new-session -d -n "Bracer-AlgorithmAPI" gunicorn3 -w 4 -b 127.0.0.1:5001 --timeout 120 --chdir /home/wcs/Bracer/Bracer-Algorithm/Bracer-Algorithm handlerapi:app
